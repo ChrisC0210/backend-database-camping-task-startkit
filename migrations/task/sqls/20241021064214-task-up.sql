691539986ea695f764d1d4b3628220d25a601b88
Chris C. <chiandeye@gmail.com>
2024-12-10 06:15:56 +0800

    test

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -12,10 +12,8 @@
 --     4. 用戶名稱為`好野人`，Email 為`richman@hexschooltest.io`，Role為`USER`
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
 --     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
-INSERT INTO
-  "USER" (name, email, role)
-VALUES
-  ('李燕容', 'lee2000@hexschooltest.io', 'USER'),
+INSERT INTO "USER" (name, email, role)
+VALUES ("李燕容", "lee2000@hexschooltest.io", "USER"),
   ('王小明', 'wXlTq@hexschooltest.io', 'USER'),
   ('肌肉棒子', 'muscle@hexschooltest.io', 'USER'),
   ('好野人', 'richman@hexschooltest.io', 'USER'),
@@ -23,26 +21,19 @@ VALUES
   ('透明人', 'opcatiy0@hexschooltest.io', 'USER');
 
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
-UPDATE
-  "USER"
-SET
-  ROLE = 'COACH'
+UPDATE "USER"
+SET "role" = 'COACH'
 WHERE
   email = 'lee2000@hexschooltest.io'
-UPDATE
-  "USER"
-SET
-  ROLE = 'COACH'
+UPDATE "USER"
+SET "role" = 'COACH'
 WHERE
   email = 'muscle@hexschooltest.io'
-UPDATE
-  "USER"
-SET
-  ROLE = 'COACH'
+UPDATE "USER"
+SET "role" = 'COACH'
 WHERE
   email = 'starplatinum@hexschooltest.io' -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
-DELETE FROM
-  "USER"
+DELETE FROM "USER"
 WHERE
   email = 'opcatiy0@hexschooltest.io' -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
 SELECT
